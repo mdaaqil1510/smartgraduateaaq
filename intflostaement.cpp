a9c33d77d278e7823ad9ee548b3cf92b024d4b64
Mohamed Aaqil <112712004+mdaaqil1510@users.noreply.github.com>
2022-09-06 18:52:23 +0530

    Delete intflostaement.cpp

--- intflostaement.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-int a;
-float b;
-printf("Please enter a int and float : ");
-scanf("%d%f",&a,&b);
-printf("The value of int is :%d and the value of float is..:%f",a,b);
-}
