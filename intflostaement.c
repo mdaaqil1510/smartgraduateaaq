00d07db501a3646d48077d882b6f8606c1521a1c
Mohamed Aaqil <112712004+mdaaqil1510@users.noreply.github.com>
2022-09-06 18:55:23 +0530

    Add files via upload

---
+++ intflostaement.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a;
+float b;
+printf("Please enter a int and float : ");
+scanf("%d%f",&a,&b);
+printf("The value of int is :%d and the value of float is..:%f",a,b);
+}
